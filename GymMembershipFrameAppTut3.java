ffef4a2f526b02f918ce04917daff40e8c1dd376
Bophelo625 <224220367@tut4life.ac.za>
2024-08-14 02:46:18 +0200

    Add files via upload

---
+++ GymMembershipFrameAppTut3.java
@@ -0,0 +1,15 @@
+
+package gymmembershipframeapptut3;
+
+import za.ac.tut.ui.GymMembershipFrameTut3;
+
+
+public class GymMembershipFrameAppTut3 {
+
+    
+    public static void main(String[] args)
+    {
+       new GymMembershipFrameTut3();
+    }
+    
+}
---
+++ GymMembershipFrameLibraryTut3.java
@@ -0,0 +1,13 @@
+
+package gymmembershipframelibrarytut3;
+
+
+public class GymMembershipFrameLibraryTut3 {
+
+   
+    public static void main(String[] args) 
+    {
+        
+    }
+    
+}
